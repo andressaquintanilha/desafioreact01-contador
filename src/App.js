8efc0d05108c28659e2c7f5976b21203d9170c73
andressaquintanilha.email <quintanilhalg3@gmail.com>
2022-02-15 08:03:55 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+////Desenvolva um contador que tenha as seguintes funcionalidades:
+//1 - O contador não pode chegar abaixo de 0
+//2 - O contador não pode chegar acima de 10
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import './App.css'
+
+class App extends Component {
+  state = {
+    count: 0,
+    msg:""
+  };
+
+  add = () => {
+    const {count} = this.state
+       if (count < 10) {
+         this.setState({
+           count: count + 1
+         })
+       }
+  };
+
+  remove = () => {
+    const {count} = this.state
+    if (count > 0) {
+      this.setState({
+        count: count - 1
+      })
+    }
+  };
+
+  render() {
+    const {count} = this.state
+    const {add, remove} = this
+    return (
+      <div className="container">
+        <section className="box">
+        <h1>React Counter App</h1>
+        <div className="counter">
+        <button onClick={add}>Adicionar</button>
+        <h2>{count}</h2>
+        <button onClick={remove}>Subtrair</button>
+        </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
